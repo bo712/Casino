422e2f89566617891b5f564065767b806499cdea
Константин <linuksoid@gmail.com>
2019-09-25 13:05:09 +0300

    fix uTests

--- Casino.Tests/Drunkard/DeckTests.cs
+++ Casino.Tests/Drunkard/DeckTests.cs
@@ -7,20 +7,14 @@ namespace Casino.Tests
     [TestFixture]
     public class DeckTests
     {
-        Deck decks;
-        [SetUp]
-        public void Setup()
-        {
-            decks = new Deck();
-        }
-
         [Test]
         public void FillDeck_10itemsEmptyArrray_SortedArray1to10()
         {
-            int size = 10;
-            int[] array = new int[10];
-            int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int size = 20;
+            int[] array = new int[size];
+            int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
             bool filledGood = true;
+
             Deck.FillDeck(array, size);
             for (int i = 0; i < array.Length; i++)
             {
@@ -36,10 +30,11 @@ namespace Casino.Tests
         [Test]
         public void ShakeDeck_SortedArray_UnsortedArrayWithSameItems()
         {
+            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
             bool allCardsInDeck = true;
-            for (int i = 0; i < decks.cards.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                if (!decks.cards.Contains(i))
+                if (!array.Contains(i))
                 {
                     allCardsInDeck = false;
                     break;
